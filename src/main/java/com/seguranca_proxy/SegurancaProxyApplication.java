99d1226367a4e32668a03094f9a04329c56d8823
devopMarkz <marcosdev2002@gmail.com>
2026-04-19 21:36:37 -0300

    first commit

---
+++ src/main/java/com/seguranca_proxy/SegurancaProxyApplication.java
@@ -0,0 +1,13 @@
+package com.seguranca_proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegurancaProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SegurancaProxyApplication.class, args);
+	}
+
+}
