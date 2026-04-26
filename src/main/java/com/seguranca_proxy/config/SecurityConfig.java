106aa1473e4db78fcf5b8aec84ec35a5429cc61d
devopMarkz <marcosdev2002@gmail.com>
2026-04-26 01:32:09 -0300

    Definindo a ordem de execucao de filtros durante requisicao

--- src/main/java/com/seguranca_proxy/config/SecurityConfig.java
+++ src/main/java/com/seguranca_proxy/config/SecurityConfig.java
@@ -3,8 +3,10 @@ package com.seguranca_proxy.config;
 import com.seguranca_proxy.security.*;
 import com.seguranca_proxy.security.enums.AccessEndpoint;
 import com.seguranca_proxy.security.filter.CustomSecurityEndpointsFilter;
+import com.seguranca_proxy.security.filter.SecurityFilter;
 import com.seguranca_proxy.security.impl.CustomAuthenticationManager;
 import com.seguranca_proxy.security.impl.UserService;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -22,6 +24,22 @@ public class SecurityConfig {
     @Bean
     public AuthenticationManager getAuthenticationManager(AuthenticationProvider authenticationProvider) {
         return new CustomAuthenticationManager(authenticationProvider);
+    }
+
+    @Bean
+    public FilterRegistrationBean<SecurityFilter> securityFilterRegistration(SecurityFilter filter) {
+        FilterRegistrationBean<SecurityFilter> registration = new FilterRegistrationBean<>();
+        registration.setFilter(filter);
+        registration.setOrder(1);
+        return registration;
+    }
+
+    @Bean
+    public FilterRegistrationBean<CustomSecurityEndpointsFilter> endpointsFilterRegistration(CustomSecurityEndpointsFilter filter) {
+        FilterRegistrationBean<CustomSecurityEndpointsFilter> registration = new FilterRegistrationBean<>();
+        registration.setFilter(filter);
+        registration.setOrder(2);
+        return registration;
     }
 
 }
